85e878a3977202ae9aa5dd70cf4246be142c66e7
Fabienne <fortunio.fabienne@gmail.com>
2019-11-25 18:31:03 +0100

    finito

---
+++ main.js
@@ -0,0 +1,10 @@
+
+$('.header-right a').click(function() {
+    $('.hamburger-menu').show(); //mostriamo il menu quando facciamo click
+});
+$('.hamburger-menu a').click(function() {
+    $('.hamburger-menu').hide(); //menu si chiude facendo click sulla X
+})
+
+//possiamo pure inserire la " i " invece di prendere " a "
+//prova git
